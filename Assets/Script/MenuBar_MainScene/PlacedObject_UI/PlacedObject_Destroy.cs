937c65fff5498c203875e90a497b767977eb4729
Tucker <ohoka.taka@icloud.com>
2025-09-19 21:33:58 +0900

    オブジェクトの破棄機能の追加

--- Assets/Script/MenuBar_MainScene/PlacedObject_UI/PlacedObject_Destroy.cs
+++ Assets/Script/MenuBar_MainScene/PlacedObject_UI/PlacedObject_Destroy.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
-using Photon.Realtime;
 using UnityEngine;
 
 
-// MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
-public class PlacedObject_Destroy : MonoBehaviourPunCallbacks
+public class PlacedObject_Destroy : MonoBehaviour
 {
 
     public void Destroy()
     {
         GameObject obj = SelectedObject.obj;
-        PlacedObject_Ownership p_o = obj.GetComponent<PlacedObject_Ownership>();
-        p_o.Change();
-        PhotonNetwork.Destroy(obj);
-        SelectedObject.obj = null;
+
+        if(obj != null)
+        {
+            //所有権の変更
+            PlacedObject_Ownership p_o = obj.GetComponent<PlacedObject_Ownership>();
+            p_o.Change();
+
+            //オブジェクトの破棄
+            PhotonNetwork.Destroy(obj);
+            SelectedObject.obj = null;
+        }
+
+        //メニューバーの遷移
+        MenuBar_Transition m_t = GetComponent<MenuBar_Transition>();
+        m_t.Transition();
 
     }
 
